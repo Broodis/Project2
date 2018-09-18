05e32564f856e7dec82930a8efe3b7be9f73ef2b
Ruslan Myaskovskiy <russ.myaskovskiy@gmail.com>
2018-09-17 19:48:35 -0700

    api routes correction

--- models/connections.js
+++ models/connections.js
@@ -6,7 +6,7 @@ var connection = mysql.createPool ({
     connectionLimit: 50,
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: '',
     database: 'keepInTouch'
 });
 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,21 +2,21 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all users
-  app.get("/api/users", function(req, res) {
+  app.get("/api/users.js", function(req, res) {
     db.Users.findAll({}).then(function(dbUsers) {
       res.json(dbUsers);
     });
   });
 
   // Create a new user
-  app.post("/api/users", function(req, res) {
+  app.post("/api/users.js", function(req, res) {
     db.Users.create(req.body).then(function(dbUsers) {
       res.json(dbUsers);
     });
   });
 
   // Delete a user by id
-  app.delete("/api/users/:id", function(req, res) {
+  app.delete("/api/users.js/:id", function(req, res) {
     db.Users.destroy({ where: { id: req.params.id } }).then(function(dbUsers) {
       res.json(dbUsers);
     });
