7a1400db3c77251bc3aff57e51d41b4afec4968a
Danny Ray <dannymorrismusic@gmail.com>
2018-09-19 21:49:45 -0700

    seperate layout file

--- public/js/home.js
+++ public/js/home.js
@@ -3,18 +3,19 @@ $(function() {
     $("#searchSubmit").on("click", function(event) {
 
         console.log('clicked');
-    // $.ajax("/api/users/search", {
-    //     type: "POST",
-    //     data: { phone:"555-555-5555"},
-    // }).then(function(response) {
-    //         if(response) {
-    //             // window.touch.href="/socials/"
-    //             window.location = "/socials/";
-    //         }
-    //         else {
-    //             window.location = "/404/";
-    //         }
-    //     })
+        
+    $.ajax("/api/users/search", {
+        type: "POST",
+        data: { phone: $("#searchInput").val() },
+    }).then(function(response) {
+            if(response) {
+                // window.touch.href="/socials/"
+                window.location = "/socials/" + response.id;
+            }
+            else {
+                window.location = "/404/";
+            }
+        })
     })
 });
 
