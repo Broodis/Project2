a08731a263355d203e4478ecadd57fedc2db8b6e
Danny Ray <dannymorrismusic@gmail.com>
2018-09-19 21:19:07 -0700

    fixes the build

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,13 +3,6 @@ var db = require("../models");
 module.exports = function(app) {
   // Get all users
   app.get("/api/users", function(req, res) {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> f88146ecb9ff21d054b31806c776e486b86dcb03
->>>>>>> 7894b9166f1e56c8f62d9085f83608a53d976b2f
     db.Users.findAll({}).then(function(dbUsers) {
       res.json(dbUsers);
       res.redirect("/signup2");
@@ -18,10 +11,6 @@ module.exports = function(app) {
 
   // Create a new user
   app.post("/api/users", function(req, res) {
-<<<<<<< HEAD
-=======
-    console.log(req.body);
->>>>>>> f88146ecb9ff21d054b31806c776e486b86dcb03
     db.Users.create(req.body).then(function(dbUsers) {
       res.redirect("/signup2");
     });
